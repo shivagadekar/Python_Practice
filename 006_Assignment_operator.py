205d93e2f77d3894ae9f4780ef3aa260ad8093b1
Shivam Gadekar <55311297+shivagadekar@users.noreply.github.com>
2023-01-17 22:06:42 +0530

    Add files via upload

---
+++ 006_Assignment_operator.py
@@ -0,0 +1,15 @@
+# ==========================================================================================================
+# '=' Equal to operator
+x = 5  # Assign 5 in x
+
+# '+=' Addition Assignment operator
+x += 1  # Equal to x = x + 1
+
+# '-=' Subtraction Assignment operator
+x -= 1  # Equal to x = x - 1
+
+# '*=' Multiplication Assignment operator
+x *= 1  # Equal to x = x * 1
+
+# '/=' Division Assignment operator
+x /= 1  # Equal to x = x / 1
