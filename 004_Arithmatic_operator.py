c0f526dad5d480d3bb6555dcfe5a178da53d5890
Shivam Gadekar <55311297+shivagadekar@users.noreply.github.com>
2023-01-17 22:06:27 +0530

    Add files via upload

---
+++ 004_Arithmatic_operator.py
@@ -0,0 +1,27 @@
+# ==========================================================================================================
+# Python Arithmetic Operators
+
+x = 21
+y = 5
+z = 3
+
+addition = x + y  # Addition operator
+print(addition)
+
+subtraction = x - y  # Subtraction Operator
+print(subtraction)
+
+multiplication = x * y  # Multiplication Operator
+print(multiplication)
+
+division = x / y  # Division Operator
+print(division)
+
+reminder = x % y  # Always Giver reminder
+print(reminder)
+
+power = x ** 2.5  # Y times X
+print(power)
+
+quotient = x // y  # Giver perfect number
+print(quotient)
---
+++ 005_Comparison_operator.py
@@ -0,0 +1,41 @@
+# ==========================================================================================================
+x = 21
+y = 5
+z = 21
+
+# Comparison operator gives output in form of 'True or False' Only.
+# It is mostly used in while loop, in if else statements.
+# = Assign
+# == is equal to
+
+print(x == y)  # False
+print(x == z)  # True
+
+print(x != y)  # True
+print(x != z)  # False
+
+print(x > y)  # True
+print(x > z)  # False
+
+print(x < y)  # False
+print(x < z)  # False
+
+print(x >= y)  # True
+print(x >= z)  # True
+
+print(x <= y)  # False
+print(x <= z)  # True
+
+print(x >= y and x >= z)  # True (Both need to satisfy)
+
+print(x > y or x > z)  # True (Only One need to Satisfy)
+
+print(not False)  # True  (Opposite of Given Condition), Used in while loop
+
+print(14 >= 14)  # True
+print(14 > 14)  # False
+print(20 >= 16)  # True
+
+print(14 <= 14)  # True
+print(14 < 14)  # False
+print(20 <= 18)  # False
